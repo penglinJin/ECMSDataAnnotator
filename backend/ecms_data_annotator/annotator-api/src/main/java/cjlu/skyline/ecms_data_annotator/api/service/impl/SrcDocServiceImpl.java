9854dd407491672ed53ff0523decfebbf05c20bb
jinpenglin <nsp4289@autuni.ac.nz>
2021-03-06 16:50:46 +0800

    fix text format issue

--- backend/ecms_data_annotator/annotator-api/src/main/java/cjlu/skyline/ecms_data_annotator/api/service/impl/SrcDocServiceImpl.java
+++ backend/ecms_data_annotator/annotator-api/src/main/java/cjlu/skyline/ecms_data_annotator/api/service/impl/SrcDocServiceImpl.java
@@ -17,6 +17,7 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -101,7 +102,7 @@ public class SrcDocServiceImpl extends ServiceImpl<SrcDocDao, SrcDocEntity> impl
                 url = new URL(filePath);
 
                 URLConnection conn = url.openConnection();
-                BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8));
 
                 String current;
                 while ((current = in.readLine()) != null) {
